d62c5a01d09af2c53b48014e25920218158e7777
Sonay Kara <zonaykara@gmail.com>
2024-10-12 15:34:09 +0300

    created-layout

---
+++ src/layouts/App/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const AppLayout = () => {
+  return (
+    <div>
+        app layout
+        <Outlet/>
+    </div>
+  )
+}
+
+export default AppLayout
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,23 +2,29 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Explore from "../pages/expolore/Explore";
 import Notifications from "../pages/notifications/Notifications";
+import AppLayout from "../layouts/App/AppLayout";
 
-const routes = createBrowserRouter ([
-    {
-        path: "/",
-        element: <Home/>
-    }, 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
 
-    {
-        path: "/explore",
-        element: <Explore/>
-    },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
 
-    {
-        path: "/notifications",
-        element: <Notifications/>
-    }
+      {
+        path: "notifications",
+        element: <Notifications />,
+      },
+    ],
+  },
+]);
 
-])
-
-export default routes
+export default routes;
